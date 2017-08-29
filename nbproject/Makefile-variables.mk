da30e0b6f914c88df0125656de6f97ad16523829
Daniele Porcelli <d.porcelli@studenti.uniba.it>
2017-08-29 12:59:03 +0200

    -Caricato il progetto su GitHub.

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=Cygwin-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin-Windows
+CND_ARTIFACT_NAME_Debug=gestorescarpe
+CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin-Windows/gestorescarpe
+CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin-Windows/package
+CND_PACKAGE_NAME_Debug=gestorescarpe.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin-Windows/package/gestorescarpe.tar
+# Release configuration
+CND_PLATFORM_Release=Cygwin-Windows
+CND_ARTIFACT_DIR_Release=dist/Release/Cygwin-Windows
+CND_ARTIFACT_NAME_Release=gestorescarpe
+CND_ARTIFACT_PATH_Release=dist/Release/Cygwin-Windows/gestorescarpe
+CND_PACKAGE_DIR_Release=dist/Release/Cygwin-Windows/package
+CND_PACKAGE_NAME_Release=gestorescarpe.tar
+CND_PACKAGE_PATH_Release=dist/Release/Cygwin-Windows/package/gestorescarpe.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
